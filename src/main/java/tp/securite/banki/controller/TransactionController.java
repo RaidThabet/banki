64e74418f795ef919feab07842df0435f923471c
Raid Thabet <thabet.raid123@gmail.com>
2025-12-14 15:39:49 +0100

    Update transaction endpoint path to include 'accounts' for better clarity

--- src/main/java/tp/securite/banki/controller/TransactionController.java
+++ src/main/java/tp/securite/banki/controller/TransactionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("{account_id}/transactions")
+@RequestMapping("accounts/{account_id}/transactions")
 @RequiredArgsConstructor
 public class TransactionController {
 
