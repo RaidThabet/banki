283519a3fabe836c3864d18a157a838de058b1d4
Raid Thabet <thabet.raid123@gmail.com>
2025-12-14 17:43:29 +0100

    Refactor Swagger error schemas to enhance clarity and provide detailed examples in API responses

--- src/main/java/tp/securite/banki/config/SwaggerConfig.java
+++ src/main/java/tp/securite/banki/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Content;
-import io.swagger.v3.oas.models.media.IntegerSchema;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
@@ -14,6 +13,10 @@ import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,28 +24,39 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openApiSpec() {
         return new OpenAPI().components(new Components()
-                .addSchemas("ApiErrorResponse", new ObjectSchema()
-                        .addProperty("status", new IntegerSchema())
-                        .addProperty("code", new StringSchema())
-                        .addProperty("message", new StringSchema())
-                        .addProperty("fieldErrors", new ArraySchema().items(
-                                new Schema<ArraySchema>().$ref("ApiFieldError"))))
-                .addSchemas("ApiFieldError", new ObjectSchema()
-                        .addProperty("code", new StringSchema())
-                        .addProperty("message", new StringSchema())
-                        .addProperty("property", new StringSchema())
-                        .addProperty("rejectedValue", new ObjectSchema())
-                        .addProperty("path", new StringSchema())));
+                .addSchemas("ErrorResponse", new ObjectSchema()
+                        .addProperty("message", new StringSchema().example("Error message"))
+                        .addProperty("code", new StringSchema().example("ERROR_CODE"))
+                        .addProperty("validationErrors", new ArraySchema().items(
+                                new ObjectSchema()
+                                        .addProperty("field", new StringSchema().example("fieldName"))
+                                        .addProperty("code", new StringSchema().example("CODE"))
+                                        .addProperty("message", new StringSchema().example("Validation error message"))))));
     }
+
 
     @Bean
     public OperationCustomizer operationCustomizer() {
-        // add error type to each operation
         return (operation, handlerMethod) -> {
+            Map<String, Object> errorExample = new HashMap<>();
+            errorExample.put("message", "Error message");
+            errorExample.put("code", "ERROR_CODE");
+
+            Map<String, String> ve = new HashMap<>();
+            ve.put("field", "fieldName");
+            ve.put("code", "CODE");
+            ve.put("message", "Validation error message");
+
+            List<Map<String, String>> veList = new ArrayList<>();
+            veList.add(ve);
+
+            errorExample.put("validationErrors", veList);
+
             operation.getResponses().addApiResponse("4xx/5xx", new ApiResponse()
                     .description("Error")
-                    .content(new Content().addMediaType("*/*", new MediaType().schema(
-                            new Schema<MediaType>().$ref("ApiErrorResponse")))));
+                    .content(new Content().addMediaType("application/json", new MediaType()
+                            .schema(new Schema<>().$ref("ErrorResponse"))
+                            .example(errorExample))));
             return operation;
         };
     }
