8a1ebe829608ce2015d5efc7f3b1e5b4a7ccddee
Raid Thabet <thabet.raid123@gmail.com>
2025-12-15 20:45:49 +0100

    Add audit log functionality to transaction creation process

--- src/main/java/tp/securite/banki/service/TransactionService.java
+++ src/main/java/tp/securite/banki/service/TransactionService.java
@@ -1,18 +1,23 @@
 package tp.securite.banki.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import tp.securite.banki.domain.Account;
+import tp.securite.banki.domain.AuditLog;
 import tp.securite.banki.domain.Transaction;
+import tp.securite.banki.domain.User;
 import tp.securite.banki.exceptions.BusinessException;
 import tp.securite.banki.exceptions.ErrorCode;
 import tp.securite.banki.model.AccountStatus;
 import tp.securite.banki.model.TransactionStatus;
 import tp.securite.banki.model.TransactionType;
 import tp.securite.banki.repos.AccountRepository;
+import tp.securite.banki.repos.AuditLogRepository;
 import tp.securite.banki.repos.TransactionRepository;
+import tp.securite.banki.util.IpAddressExtractor;
 
 import java.util.List;
 import java.util.Random;
@@ -22,8 +27,10 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TransactionService {
 
+    private final IpAddressExtractor ipAddressExtractor;
     private final TransactionRepository transactionRepository;
     private final AccountRepository accountRepository;
+    private final AuditLogRepository auditLogRepository;
 
     public List<Transaction> listTransactionsForAccount( UUID userID, UUID accountId) {
         Account account = accountRepository.findAccountsByIdAndOwner_Id(accountId, userID)
@@ -39,7 +46,8 @@ public class TransactionService {
     public Transaction createTransactionForUser(
             UUID accountId,
             UUID beneficiaryId,
-            double amount
+            double amount,
+            HttpServletRequest request
     ) {
         Account userAccount = accountRepository.findById(accountId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.ACCOUNT_NOT_FOUND, accountId));
@@ -79,7 +87,23 @@ public class TransactionService {
         accountRepository.saveAll(List.of(userAccount, beneficiaryAccount));
         transactionRepository.save(creditTransaction);
 
-        return transactionRepository.save(debitTransaction);
+        Transaction createdTransaction = transactionRepository.save(debitTransaction);
+
+        User owner = userAccount.getOwner();
+
+        String ipAddress = ipAddressExtractor.getClientIpAddress(request);
+        String auditLogMessage = "User with id %s made a new transaction".formatted(owner.getId());
+
+        AuditLog auditLog = AuditLog.builder()
+                .userId(owner)
+                .action("CREATE_TRANSACTION")
+                .ipAddress(ipAddress)
+                .details(auditLogMessage)
+                .build();
+
+        auditLogRepository.save(auditLog);
+
+        return createdTransaction;
     }
 
     @Scheduled(fixedDelay = 60000) // run every 60 seconds
